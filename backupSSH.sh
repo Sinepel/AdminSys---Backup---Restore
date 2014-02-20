f46b503d83197727f23d071bb76bded2f1ccfc4e
root <root@server.da2i.org>
2014-02-20 09:37:54 +0100

    Save Incrémentale, avec droits, rotation, psql

--- backupSSH.sh
+++ backupSSH.sh
@@ -10,9 +10,15 @@ USERSSH="root"
 HOSTSSH="192.168.194.27"
 # la date
 data=`date "+%Y-%m-%d-%H-%M"`
+#PSQL
+su postgres -c 'pg_dumpall -U postgres' >dump-$data.sql
+rsync -auP -H -rplog "dump-$data.sql" "-e ssh" $USERSSH@$HOSTSSH:"$DIR_BACKUP_INC/psql/dump-$data.sql"
+rm dump-$data.sql
 #Commande
-rsync -auP -H  --link-dest="../precedente" \
+rsync -auP -H -rlpog --link-dest="../precedente" \
 --exclude-from=$_DIR_BACKUP_INC/exclure.lst --delete \
 "$DIR_ORIG" "-e ssh" $USERSSH@$HOSTSSH:"$DIR_BACKUP_INC/back-$data"
 # En SSH nous allons créer les fichiers
 ssh $USERSSH@$HOSTSSH rm -f $DIR_BACKUP_INC/precedente" && "ln -s $DIR_BACKUP_INC/back-$data  $DIR_BACKUP_INC/precedente
+./rmOld.sh
+rm recupRs
--- rmOld.sh
+++ rmOld.sh
@@ -1,5 +1,8 @@
 rs=$(ssh root@192.168.194.27 'ls -t /srv/ | grep "back"')
 echo $rs>recupRs
 dernierElem=`awk -F' ' '{if(NF>3) print $(NF)}' recupRs`
-echo $dernierElem
-ssh root@192.168.194.27 'rm -rf /srv/'$dernierElem''
+echo ${#dernierElem}
+if (test ${#dernierElem} -gt 1)
+then
+	ssh root@192.168.194.27 'rm -rf /srv/'$dernierElem''
+fi
